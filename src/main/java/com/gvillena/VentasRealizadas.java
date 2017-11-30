ba897c9651366812cfa494bcbaf4d5b0634a6b29
PISCO <PISCO@NOEMI>
2017-11-30 15:50:48 -0500

    firsth

--- src/main/java/com/gvillena/VentasRealizadas.java
+++ src/main/java/com/gvillena/VentasRealizadas.java
@@ -1,7 +1,12 @@
 
 package com.gvillena;
 
-import java.util.ArrayList;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class VentasRealizadas {
   private String nombre;
@@ -10,9 +15,15 @@ public class VentasRealizadas {
   private String ciudad;
   private String pais;
   private String marca_celular;
+  
+    BufferedWriter bw ;
+    PrintWriter pw;
+    FileWriter fw;
+    File f;
+  
   //ArrayList<String> ventas= new ArrayList<>();
   public VentasRealizadas(){}
-  public VentasRealizadas(String nom,String pago,String email,String ciu,String pais,String marcacel)
+  public VentasRealizadas(String nom,String pago,String email,String ciu,String pais,String marcacel) throws IOException
   {
       this.nombre=nom;
       this.correo=email;
@@ -20,7 +31,24 @@ public class VentasRealizadas {
       this.ciudad=ciu;
       this.pais=pais;
       this.marca_celular=marcacel;
+      RegistrarVenta();
       
+  }
+  public void RegistrarVenta() throws IOException{
+      f= new File("ResgistroVentas.txt");
+      fw= new FileWriter(f);
+      bw=new BufferedWriter(fw);
+      pw= new PrintWriter(bw);
+      
+      pw.write("Nombre: "+nombre+"\r\n");      
+      pw.append("Email: "+correo+"\r\n");
+      pw.append("Pago: "+pago+"\r\n");
+      pw.append("Ciudad: "+ciudad+"\r\n");
+      pw.append("Pais: "+pais+"\r\n");
+      pw.append("Marca de Celular: "+marca_celular+"\r\n"); 
+      
+      bw.close();
+      pw.close();
   }
   
     
